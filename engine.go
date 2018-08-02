38c067de860b2621051bd66b1b6aa617a2964182
Jonathan Sudibya <jonathan@hooq.tv>
2018-08-02 07:38:40 +0700

    Add goroutine

--- engine.go
+++ engine.go
@@ -3,19 +3,20 @@ package fished
 import (
 	"encoding/hex"
 	"strings"
+	"sync"
+	"time"
 
-	"github.com/hashicorp/golang-lru"
+	"github.com/allegro/bigcache"
 	"github.com/oleksandr/conditions"
 )
 
 type (
 	// Rule ...
 	Rule struct {
-		ID                  string
-		Input               []string
-		Expression          string
-		EvaluatedExpression *conditions.Parser
-		Output              map[string]RuleOutput
+		ID         string
+		Input      []string
+		Expression string
+		Output     map[string]RuleOutput
 	}
 
 	// RuleOutput ...
@@ -36,65 +37,110 @@ type (
 	// Engine ...
 	Engine struct {
 		ruleFunctions map[string]RuleFunction
-		workingMemory *lru.Cache
-		rules         *lru.Cache
-		usedRule      []string
+		initialState  map[string]interface{}
+		workingMemory *bigcache.BigCache
+		rules         map[string]Rule
+		usedRule      map[string]interface{}
 	}
 
 	// RuleFunction ..
 	RuleFunction func(...interface{}) (interface{}, error)
 )
 
-// New ...
-func New() (*Engine, error) {
-	return &Engine{}, nil
+var (
+	// RuleBook ...
+	RuleBook map[string]*conditions.Parser
+)
+
+func init() {
+	RuleBook = make(map[string]*conditions.Parser)
 }
 
-// SetRules ...
-func (e *Engine) SetRules(r []Rule) error {
-	var err error
-
-	e.rules, err = lru.New(64 * 1024)
+// New ...
+func New() (*Engine, error) {
+	// TODO: Make eviction time configurable
+	wm, err := bigcache.NewBigCache(bigcache.DefaultConfig(10 * time.Minute))
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	e.usedRule = make([]string, len(r))
-	for _, v := range r {
-		go func() {
-			ruleBytes, _ := getBytes(v)
-			hash := getMD5Hash(ruleBytes)
-			v.ID = hex.EncodeToString(hash)
+	return &Engine{
+		initialState:  make(map[string]interface{}),
+		workingMemory: wm,
+		rules:         make(map[string]Rule),
+	}, nil
+}
 
-			v.EvaluatedExpression = conditions.NewParser(strings.NewReader(v.Expression))
-
-			e.rules.Add(v.ID, v)
-		}()
+// SetFacts ...
+func (e *Engine) SetFacts(facts map[string]interface{}) error {
+	for k, v := range facts {
+		e.initialState[k] = v
 	}
 	return nil
 }
 
-// SetFacts ...
-func (e *Engine) SetFacts(f map[string]interface{}) error {
-	var err error
-	e.workingMemory, err = lru.New(1024)
-	if err != nil {
-		return err
-	}
+// SetRules ...
+func (e *Engine) SetRules(rules []Rule) error {
+	var wg sync.WaitGroup
+	e.rules = make(map[string]Rule)
+	e.usedRule = make(map[string]interface{})
+	for _, v := range rules {
+		wg.Add(1)
+		go func() {
+			ruleHash := hex.EncodeToString(getMD5Hash([]byte(v.Expression)))
+			v.ID = ruleHash
 
-	for k, v := range f {
-		e.workingMemory.Add(k, v)
+			parsedExpression := conditions.NewParser(strings.NewReader(v.Expression))
+
+			if _, ok := RuleBook[ruleHash]; !ok {
+				RuleBook[ruleHash] = parsedExpression
+			}
+			e.rules[ruleHash] = v
+			wg.Done()
+		}()
 	}
+	wg.Wait()
 	return nil
 }
 
 // SetRuleFunctions ...
 func (e *Engine) SetRuleFunctions(rf map[string]RuleFunction) error {
-	e.ruleFunctions = make(map[string]RuleFunction)
-
 	for k, v := range rf {
 		e.ruleFunctions[k] = v
 	}
 
 	return nil
+}
+
+// Run ...
+func (e *Engine) Run() error {
+	task := make(chan string)
+	queue := make(chan string)
+	done := make(chan bool)
+
+	go taskMaster(task, queue, done)
+
+	// Copy initial state to working memory
+	var wg sync.WaitGroup
+	for k, v := range e.initialState {
+		wg.Add(1)
+		go func() {
+			b, _ := getBytes(v)
+			e.workingMemory.Set(k, b)
+			task <- k
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	return nil
+}
+
+func taskMaster(task, queue chan string, done chan bool) {
+	select {
+	case <-task:
+		// do task
+	case <-done:
+		return
+	}
 }
